babe21e22b32a0a11ae59654f1b4a5927a675771
Chris Uzoh <genbliz@gmail.com>
2023-03-24 03:22:16 +0100

    up server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import app from "./app";
 import http from "http";
 import { envConfig } from "./helpers/env";
+import { LoggingService } from "./helpers/logging-service";
 
 const PORT = envConfig.PORT || 3000;
 const server = http.createServer(app);
+
+process.on("unhandledRejection", (reason, promise) => {
+  LoggingService.error(reason);
+});
+
+if (envConfig.NODE_ENV !== "production") {
+  process.on("warning", (warning) => {
+    LoggingService.warn(warning);
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
